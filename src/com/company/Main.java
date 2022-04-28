94cdd26d05ecfbd3367597af1e8e31fe5a0fd698
Viktoriya <bychenVI@gmail.com>
2022-04-28 22:32:16 +0300

    added sum from 0 to 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         int i = 0;
-	// write your code here
+        int sum = 0;
+        for (int j = 0; j < 10; j++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
